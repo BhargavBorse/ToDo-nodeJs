5f3c8d40a6492c69cb332433acfb86af4e99fb33
Bhargav Borse <bhargavborse4898@gmail.com>
2020-06-22 11:43:52 +0530

    updated localhost - not working

--- app.js
+++ app.js
@@ -13,5 +13,5 @@ app.use(express.static('./public'));
 todoController(app); 
 
 //listening to port
-app.listen(3000);
-console.log("Listening to port 3000");
+app.listen(2000);
+console.log("Listening to port 2000");
